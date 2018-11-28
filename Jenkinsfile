c81b8fec2953116fc0eeac21be4a950edb719f2c
DylanLennard <dlennard09@yahoo.com>
2018-11-27 18:49:06 -0800

    updated changes to test file and to Jenkinsfile to try to initiate a real build here

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,27 @@
 pipeline {
     agent any 
     stages {
-        stage('Test') {
+        stage('Build Image') {
             agent any 
             steps {
-                echo 'Jenkins, are you there?...'
+                sh 'git clone https://github.com/DylanLennard/after-tax-income-service.git'
+                sh 'cd after-tax-income-service/'
+                sh 'docker build -t testrepo .'
+                sh 'docker tag testrepo testrepo:testtag'
+            }
+        }
+        stage('Unit Tests') {
+            agent docker {image python3.6-slim}
+            steps {
+                sh 'pip3 install -r requirements.txt'
+                sh 'python -m unittest test.py'
+                # figure out how to signify that these tests passed or nah
+            }
+        }
+        stage('Deploy') {
+            agent any
+            steps{
+                  echo 'Push Build to ECR...'
             }
         }
     }
--- test.py
+++ test.py
@@ -4,27 +4,36 @@ import tax_app
 class TestRawMethods(unittest.TestCase):
 
     INCOME = 50_000
+    FED_INCOME_TAX = 6_939.38
+    STATE_INCOME_TAX = 2_107.47
+    OTHER_INCOME_TAX_NOT_SELF_EMP = 3_825.00
+    OTHER_INCOME_TAX_SELF_EMP = 7_650.00
+    TOTAL_EXPECTED_TAX = 37_128.15
 
     def test_fed_tax(self):
+        """Test that the federal tax function returns the correct value"""
         fed_taxes = tax_app.federal_tax(TestRawMethods.INCOME)
-        self.assertEqual(6_939.38, fed_taxes)
+        self.assertEqual(TestRawMethods.FED_INCOME_TAX, fed_taxes)
 
     def test_state_tax(self):
+        """Test that the state tax function returns correct value"""
         state_taxes = tax_app.state_tax(TestRawMethods.INCOME)
-        self.assertEqual(2_107.47, state_taxes)
+        self.assertEqual(TestRawMethods.STATE_INCOME_TAX, state_taxes)
 
     def test_other_tax(self):
+        """Test that other_tax function returns correct value given all parameters"""
         # test when status true
         other_taxes_status_true = tax_app.other_tax(TestRawMethods.INCOME, True)
-        self.assertEqual(3_825.00, other_taxes_status_true)
+        self.assertEqual(TestRawMethods.OTHER_INCOME_TAX_NOT_SELF_EMP, other_taxes_status_true)
 
         # test when status is False
         other_taxes_status_false = tax_app.other_tax(TestRawMethods.INCOME, False)
-        self.assertEqual(7_650.00, other_taxes_status_false)
+        self.assertEqual(TestRawMethods.OTHER_INCOME_TAX_SELF_EMP, other_taxes_status_false)
 
     def test_tax(self):
+        """Test that the main function as a whole works"""
         tax = tax_app.tax(TestRawMethods.INCOME)
-        self.assertEqual(37_128.15, tax)
+        self.assertEqual(TestRawMethods.TOTAL_EXPECTED_TAX, tax)
 
 
 if __name__ == '__main__':
