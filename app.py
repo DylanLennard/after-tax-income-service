eaee930f7587d9943223139b310452084937dd53
DylanLennard <dlennard09@yahoo.com>
2019-02-12 19:23:23 -0800

    02/12/19 7:22PM || Made the output of the handler into JSON and changed port to 8080 since no longer used for a real service

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import Flask
 from flask import request
 import tax_app
+import json
 app = Flask(__name__)
 
 # TODO: use json to return an object with all info
-
 @app.route('/')
 def hello_world():
     return 'Aye!'
+
 
 @app.route('/after_tax_income')
 def after_tax_income():
     income = float(request.args.get('income'))
     status = request.args.get('status')
-    after_tax = str(tax_app.tax(income, status))
-    return after_tax
+    after_tax = dict(after_tax_income=tax_app.tax(income, status))
+    return json.dumps(after_tax)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80)
+    app.run(host="0.0.0.0", port=8080)
